994efb6de0ab5e0125c3b84c31c6e8433afd8c11
mathuradon <mathu.dahal@hotmail.com>
2019-08-20 22:56:21 -0500

    First Commit

---
+++ src/main/java/com/usaa/deposit/IntellijJavaApplication.java
@@ -0,0 +1,13 @@
+package com.usaa.deposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijJavaApplication.class, args);
+	}
+
+}
